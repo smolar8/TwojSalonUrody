0c717fe24275cdd338e33c09cf8194fa7f7bbf41
Volodymyr <coremio3@gmail.com>
2022-12-24 13:52:50 +0100

    start preparing inform about title

--- src/components/oferta/Oferta.jsx
+++ src/components/oferta/Oferta.jsx
@@ -1,28 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styleOferta/oferta.css";
-import testimg from "../../img&icon/manicure.jpg";
-
+import j from "../../img&icon/1head.jpg";
+import ofertaJson from "../json/oferta.json";
+const URL = "/static/media/";
 export default function Oferta() {
+  const [oferta, setOferta] = useState({
+    id: "",
+    title: "",
+    img: "",
+  });
+
+  const handleVisibleOferta = (e) => {
+    e.preventDefault();
+    ofertaJson.map((item) => {
+      return e.target.innerText === item.title ? setOferta(item) : null;
+    });
+  };
+
   return (
     <div className="wrapper--oferta">
-      <div className="first">
-        <p className="oferta--p">Pielęgnacja twarzy, szyi i dekoltu</p>
-      </div>
-      <p className="oferta--p">Pielęgnacja twarzy, szyi i dekoltu</p>
-      <p className="oferta--p">Pielęgnacja dłoni i paznokci</p>
-      <p className="oferta--p">
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Pielęgnacja twarzy, szyi i dekoltu
+      </p>
+
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Pielęgnacja twarzy, szyi i dekoltu
+      </p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Pielęgnacja dłoni i paznokci
+      </p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
         Pielęgnacja stóp - <b>PODOLOGIA</b>
       </p>
-      <p className="oferta--p">Pielęgnacja ciała</p>
-      <p className="oferta--p">Medycyne estetyczna</p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Pielęgnacja ciała
+      </p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Medycyne estetyczna
+      </p>
 
-      <p className="oferta--p">Pielęgnacja brwi i rzęs</p>
-      <p className="oferta--p">Wizaź</p>
-      <p className="oferta--p">Depilacja</p>
-      <p className="oferta--p">Strefa wellness</p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Pielęgnacja brwi i rzęs
+      </p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Wizaź
+      </p>
+      <p className="oferta--p" onClick={handleVisibleOferta}>
+        Depilacja
+      </p>
+      <p
+        className="oferta--p"
+        name="Strefa wellness"
+        onClick={handleVisibleOferta}
+      >
+        Strefa wellness
+      </p>
 
-      <p className="first">Przekłuwanie uszu</p>
-      <img id="tettt" className="ofr-img-img" src={testimg} alt="yyy" />
+      <p
+        className="first"
+        name="Przekłuwanie uszu"
+        onClick={handleVisibleOferta}
+      >
+        Przekłuwanie uszu
+      </p>
+      <div className="ofr-img-img">
+        <img id="tettt" className="ofr-img-img" src={oferta.img} alt="yyy" />
+
+        <p>{oferta.text}</p>
+      </div>
     </div>
   );
 }
